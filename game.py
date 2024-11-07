1c675dce4a484e21a5d8ffc3dbdd84ebdf0e7345
lucila-hernandez <lucila.hernandezdelcarmen@students.dominican.edu>
2024-11-06 19:49:28 -0800

    making things move

--- game.py
+++ game.py
@@ -1,38 +1,67 @@
 # Import and initialize pygame
+from random import randint
 import pygame 
 pygame.init()
 
 # Configure the screen
 screen = pygame.display.set_mode([500, 500])
 
-# Game Object
+# Get the clock
+clock = pygame.time.Clock()
+
+# Game Object class
 class GameObject(pygame.sprite.Sprite):
-  # Remove width and height and add image here!
-  def __init__(self, x, y, image):
-    super(GameObject, self).__init__()
-    # self.surf = pygame.Surface((width, height)) REMOVE!
-    # self.surf.fill((255, 0, 255)) REMOVE!
-    self.surf = pygame.image.load(image) # ADD!
-    self.x = x
-    self.y = y
+    def __init__(self, x, y, image):
+        super(GameObject, self).__init__()
+        self.surf = pygame.image.load(image)
+        self.x = x
+        self.y = y
 
-  def render(self, screen):
-    screen.blit(self.surf, (self.x, self.y))
+    def render(self, screen):
+        screen.blit(self.surf, (self.x, self.y))
 
-# Make an instance of GameObject
-# box = GameObject(120, 300, 50, 50) REMOVE!
-apple = GameObject(120, 300, 'apple.png') # ADD!
+# Apple class
+class Apple(GameObject):
+    def __init__(self):
+        super(Apple, self).__init__(0, 0, 'apple.png')
+        self.dx = 0
+        self.dy = (randint(0, 200) / 100) + 1
+        self.reset()  # Call reset here!
 
-# Creat the game loop
+    def move(self):
+        self.x += self.dx
+        self.y += self.dy
+        # Check the y position of the apple
+        if self.y > 500:
+            self.reset()
+
+    # Add a new method
+    def reset(self):
+        self.x = randint(50, 400)
+        self.y = -64
+
+# Make an instance of Apple
+apple = Apple()
+
+# Create the game loop
 running = True
 while running:
-  # Looks at events
-  for event in pygame.event.get():
-    if event.type == pygame.QUIT:
-      running = False
-      
-  # Draw a circle
-  screen.fill((255, 255, 255))
-  # Draw box
-  apple.render(screen)
-  pygame.display.flip()
+    # Look at events
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    # Clear screen
+    screen.fill((255, 255, 255))
+    # Move and render the apple
+    apple.move()
+    apple.render(screen)
+    # Update the window
+    pygame.display.flip()
+    # Tick the clock!
+    clock.tick(60)
+
+# Quit pygame
+pygame.quit()
+
+
