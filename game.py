0456991f7cbe50a3beb17ccce813aa352ff65676
lucila-hernandez <lucila.hernandezdelcarmen@students.dominican.edu>
2024-11-06 18:58:19 -0800

    drawing objects

--- game.py
+++ game.py
@@ -5,19 +5,34 @@ pygame.init()
 # Configure the screen
 screen = pygame.display.set_mode([500, 500])
 
+# Game Object
+class GameObject(pygame.sprite.Sprite):
+  # Remove width and height and add image here!
+  def __init__(self, x, y, image):
+    super(GameObject, self).__init__()
+    # self.surf = pygame.Surface((width, height)) REMOVE!
+    # self.surf.fill((255, 0, 255)) REMOVE!
+    self.surf = pygame.image.load(image) # ADD!
+    self.x = x
+    self.y = y
+
+  def render(self, screen):
+    screen.blit(self.surf, (self.x, self.y))
+
+# Make an instance of GameObject
+# box = GameObject(120, 300, 50, 50) REMOVE!
+apple = GameObject(120, 300, 'apple.png') # ADD!
+
 # Creat the game loop
-running = True 
-while running: 
-	# Looks at events 
-	for event in pygame.event.get():
-		if event.type == pygame.QUIT:
-			running = False
-	
-	# Clear the screen
-	screen.fill((255, 255, 255))
-	# Draw a circle
-	color = (255, 0, 255)
-	position = (250, 250)
-	pygame.draw.circle(screen, color, position, 75)
-	# Update the display
-	pygame.display.flip()
+running = True
+while running:
+  # Looks at events
+  for event in pygame.event.get():
+    if event.type == pygame.QUIT:
+      running = False
+      
+  # Draw a circle
+  screen.fill((255, 255, 255))
+  # Draw box
+  apple.render(screen)
+  pygame.display.flip()
