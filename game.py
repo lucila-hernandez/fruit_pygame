045994c83d03bfb348e1ce44445fa69fe2d5542e
lucila-hernandez <lucila.hernandezdelcarmen@students.dominican.edu>
2024-11-11 15:29:42 -0800

    collisions

--- game.py
+++ game.py
@@ -8,16 +8,19 @@ lanes = [93, 218, 343]
 # Configure the screen
 screen = pygame.display.set_mode([500, 500])
 
-# Game Object class
+# Game Object
 class GameObject(pygame.sprite.Sprite):
-    def __init__(self, x, y, image):
-        super(GameObject, self).__init__()
-        self.surf = pygame.image.load(image)
-        self.x = x
-        self.y = y
+	def __init__(self, x, y, image):
+		super(GameObject, self).__init__()
+		self.surf = pygame.image.load(image)
+		self.x = x
+		self.y = y
+		self.rect = self.surf.get_rect() # add 
 
-    def render(self, screen):
-        screen.blit(self.surf, (self.x, self.y))
+	def render(self, screen):
+		self.rect.x = self.x # add
+		self.rect.y = self.y # add
+		screen.blit(self.surf, (self.x, self.y))
 
 # Apple class
 class Apple(GameObject):
@@ -55,6 +58,43 @@ class Strawberry(GameObject):
     def reset(self):
         self.x = -64
         self.y = choice(lanes)
+
+
+class Bomb(GameObject):
+  def __init__(self):
+    super(Bomb, self).__init__(0, 0, 'bomb.png')
+    self.dx = 0
+    self.dy = 0
+    self.reset()
+
+  def move(self):
+    self.x += self.dx
+    self.y += self.dy
+    if self.x > 500 or self.x < -64 or self.y > 500 or self.y < -64:
+      self.reset()
+
+  def reset(self):
+    direction = randint(1, 4)
+    if direction == 1: # left
+      self.x = -64
+      self.y = choice(lanes)
+      self.dx = (randint(0, 200) / 100) + 1
+      self.dy = 0
+    elif direction == 2: # right
+      self.x = 500
+      self.y = choice(lanes)
+      self.dx = ((randint(0, 200) / 100) + 1) * -1
+      self.dy = 0
+    elif direction == 3: # down
+      self.x = choice(lanes)
+      self.y = -64
+      self.dx = 0
+      self.dy = (randint(0, 200) / 100) + 1
+    else:
+      self.x = choice(lanes)
+      self.y = 500
+      self.dx = 0
+      self.dy = ((randint(0, 200) / 100) + 1) * -1
 
 class Player(GameObject):
     def __init__(self):
@@ -102,17 +142,26 @@ class Player(GameObject):
 # Make a group
 all_sprites = pygame.sprite.Group()
 
-# make an instance of Player
-player = Player()
+# make a fruits Group
+fruit_sprites = pygame.sprite.Group()
 
 # Make an instance of Apple
 apple = Apple()
 strawberry = Strawberry()
+fruit_sprites.add(apple)
+fruit_sprites.add(strawberry)
+
+# make an instance of Player
+player = Player()
+
+# make bomb
+bomb = Bomb()
 
 # Add sprites to group
 all_sprites.add(player)
 all_sprites.add(apple)
 all_sprites.add(strawberry)
+all_sprites.add(bomb)
 
 # Get the clock
 clock = pygame.time.Clock()
@@ -141,10 +190,17 @@ while running:
     for entity in all_sprites:
         entity.move()
         entity.render(screen)
+    # Check Colisions
+    fruit = pygame.sprite.spritecollideany(player, fruit_sprites)
+    if fruit:
+        fruit.reset()
+    # Check collision player and bomb
+    if pygame.sprite.collide_rect(player, bomb):
+        running = False
     # Update the window
     pygame.display.flip()
     # Update the window
-    clock.tick(60)
+    clock.tick(50)
 
 
 
